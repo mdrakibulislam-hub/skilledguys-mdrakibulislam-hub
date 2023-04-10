830dd752dc4c6ca6cb347fffcd1ae72c4101b6e9
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-04-10 08:38:30 +0600

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#7E90FE",
+
+          "secondary": "#F9F9FF",
+
+          "accent": "#474747",
+
+          "neutral": "#1A1919",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#8EB0F1",
+
+          "success": "#7AE6C4",
+
+          "warning": "#F6BE23",
+
+          "error": "#EC3C65",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
