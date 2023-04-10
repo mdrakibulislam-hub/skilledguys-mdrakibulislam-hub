306a3a2e73522ce37b905991abe6fb6a5c08d222
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-04-10 17:59:16 +0600

    jobs cards section done

--- src/components/JobsCard.jsx
+++ src/components/JobsCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocation, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 
 const JobsCard = ({ jobInfo }) => {
     const { _id, salary, companyLogo, companyName, jobTitle, workLocation, timeType, location, } = jobInfo
     return (
         <div
-            className='border-2 border-success p-8 rounded-md'
+            className='border-2 border-[#E8E8E8] p-8 rounded-md'
         >
             <img src={companyLogo} alt="Brand Logo" />
             <h3
-                className='text-2xl font-bold text-accent'
+                className='text-2xl font-bold text-accent mt-4'
             >{jobTitle}</h3>
             <p
                 className='text-lg font-semibold text-success'
             >{companyName}</p>
             <div
-                className='flex gap-2'
+                className='flex gap-2 mt-4'
             >
                 <div
                     className='card-outline-button'
@@ -24,10 +26,12 @@ const JobsCard = ({ jobInfo }) => {
                 >{timeType}</div>
             </div>
             <div
-                className='flex gap-2 text-base text-success font-medium'
+                className='flex gap-2 text-base text-success font-medium my-4'
             >
-                <div>{location}</div>
-                <div>{salary}</div>
+                <div>
+                    <FontAwesomeIcon icon={faLocation}></FontAwesomeIcon> {location}</div>
+                <div>
+                    <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon> {salary}</div>
             </div>
             <button
                 className='btn-primary'
--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -7,19 +7,21 @@ const JobsContainer = () => {
 
     return (
 
-        <div className='default-container'>
+        <div className='default-container flex flex-col justify-center'>
 
-            <h1
-                className='font-bold text-5xl text-center'
-            >
-                Featured Jobs
-            </h1>
+            <div>
+                <h1
+                    className='font-bold text-5xl text-center'
+                >
+                    Featured Jobs
+                </h1>
 
-            <p
-                className='font-medium text-base text-slate-500 text-center py-8'
-            >
-                Explore thousands of job opportunities with all the information you need. Its your future
-            </p>
+                <p
+                    className='font-medium text-base text-slate-500 text-center py-8'
+                >
+                    Explore thousands of job opportunities with all the information you need. Its your future
+                </p>
+            </div>
 
             <div
                 className='grid grid-cols-2 gap-8'
@@ -27,6 +29,10 @@ const JobsContainer = () => {
 
                 {jobsContext.map(jobInfo => <JobsCard key={jobInfo._id} jobInfo={jobInfo}></JobsCard>)}
             </div>
+
+            <button
+                className='btn-primary mx-auto my-16'
+            >See All</button>
 
         </div>
     );
