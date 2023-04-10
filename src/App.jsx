ea8560d5226883e2153bc0d88a4136f1f56228a8
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-04-10 12:50:23 +0600

    Banner section done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-
+import { createContext, useState } from 'react'
 import './App.css'
+import { Outlet, useLoaderData } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const allJobsContext = createContext()
 
 function App() {
 
+  const jobsData = useLoaderData()
+
+  console.log(jobsData);
   return (
     <>
-
+      <allJobsContext.Provider value={jobsData}>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </allJobsContext.Provider>
     </>
   )
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import man from '../assets/man.webp'
+
+const Banner = () => {
+    return (
+        <div className='bg-secondary'>
+            <div
+                className='default-container min-h-[100vh] md:min-h-fit py-10 lg:py-32 banner bg-no-repeat bg-contain bg-right-bottom'>
+                <div className='lg:w-1/2'>
+                    <h1
+                        className='font-bold text-5xl md:text-7xl'
+                    >
+                        Empowering job seekers <p className='text-primary'>worldwide.</p>
+                    </h1>
+                    <p
+                        className='font-medium text-base md:text-lg py-4 text-slate-500'
+                    >
+                        Maximizing your job search potential. Finding the perfect job match for you. Helping you take the next step in your career.
+                    </p>
+                    <button
+                        className='btn-primary'
+                    >
+                        Get Started
+                    </button>
+                </div>
+
+                {/* <img src={man} className='w-4/12' alt="professional middle aged man with confidence in her face." /> */}
+
+            </div>
+        </div >
+    );
+};
+
+export default Banner;
